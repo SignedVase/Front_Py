7b8d05eb556fed4b7b60678628ac07393472d316
Signedvase <augustoalves.07@outlook.com.br>
2026-04-23 15:55:42 -0300

    Testes com multiplas classes e orientacao a objetos

--- Tela_1.py
+++ Tela_1.py
@@ -1,19 +1,38 @@
 from PyQt6.QtWidgets import QApplication, QLabel, QMainWindow, QPushButton
 from PyQt6.QtCore import Qt
+class SegundaJanela(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Segunda Janela")
+        self.resize(300, 200)
+        label2 = QLabel('Bom dia', self)
+        label2.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        label2.setStyleSheet("color: black;")
+        label2.resize(300, 200)
+
+class JanelaPrin(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("janela principal")
+        self.resize(400, 300)
+        label = QLabel('Hi', self)
+        label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        label.setStyleSheet("color: black;")
+        label.resize(400, 300)
+
+        botao = QPushButton("Abrir Janela 2", self)
+        botao.move(120,120)
+        botao.clicked.connect(self.abrir_segunda)
+
+    def abrir_segunda(self):
+        self.segunda = SegundaJanela()
+        self.segunda.show()  # não bloqueia a janela principal
+
+
+
+
 
 app = QApplication([])
-
-janela = QMainWindow()
-
-label = QLabel('Oi', janela)
-label.setAlignment(Qt.AlignmentFlag.AlignCenter)
-label.setStyleSheet("color: black;")
-label.resize(400,300)
-
-botao = QPushButton("Clique Aqui", janela)
-botao.move(150,100)
-botao.resize(120,40)
-
-janela.resize(400,300)
+janela = JanelaPrin()
 janela.show()
 app.exec()
