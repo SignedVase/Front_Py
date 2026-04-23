335b9a35c7c3028d0e33e85c9bb08e20cd356d2b
Signedvase <augustoalves.07@outlook.com.br>
2026-04-23 13:47:32 -0300

    First commit

---
+++ Tela_1.py
@@ -0,0 +1,19 @@
+from PyQt6.QtWidgets import QApplication, QLabel, QMainWindow, QPushButton
+from PyQt6.QtCore import Qt
+
+app = QApplication([])
+
+janela = QMainWindow()
+
+label = QLabel('Oi', janela)
+label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+label.setStyleSheet("color: yellow;")
+label.resize(400,300)
+
+botao = QPushButton("Clique Aqui", janela)
+botao.move(150,100)
+botao.resize(120,40)
+
+janela.resize(400,300)
+janela.show()
+app.exec()
